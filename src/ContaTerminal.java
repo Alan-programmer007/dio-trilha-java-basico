24baf341987e802666d097e551070b90be69cf54
Alan da Silva <luckywall80@gmail.com>
2024-10-03 19:52:02 -0300

    Implementação do meio de recebimento de dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int numero = entrada.nextInt();
+        System.out.println("Por favor, digite a Agência !");
+        String agencia = entrada.nextLine();
+        System.out.println("Por favor, digite o Seu nome completo !");
+        String nome = entrada.nextLine();
+        System.out.println("Por favor, digite o seu saldo !");
+        double saldo = entrada.nextDouble();
     }
 }
